7b14c3a8b2c47678993c14fccb10b442decd8579
WagnerColares <luizwagner169@gmail.com>
2025-11-30 13:13:37 -0300

    envio da calculadora

---
+++ CF/Calculadora_POO/src/main/java/com/mycompany/calculadora_poo/Calculadora_POO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora_poo;
+
+/**
+ *
+ * @author User
+ */
+public class Calculadora_POO {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+               new POOcalc().setVisible(true);              
+            }
+        });
+    }
+}
